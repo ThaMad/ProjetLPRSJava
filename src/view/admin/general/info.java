fbcffbc0fdb34baf992038d5f311f9f3b8b3f37a
RyanLefebvreT <46060336+RyanLefebvreT@users.noreply.github.com>
2022-02-15 13:58:06 +0100

    Jtable administrateur

--- src/view/admin/general/info.java
+++ src/view/admin/general/info.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 
+import manager.manager_thomas;
 import view.admin.gestionUser.addUser;
 import view.admin.gestionUser.modUser;
 
@@ -13,12 +14,18 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Vector;
 import java.awt.event.ActionEvent;
 
 public class info {
 
 	private JFrame frame;
 	private JTable table;
+	private Connection connexion;
 
 	/**
 	 * Launch the application.
@@ -47,22 +54,56 @@ public class info {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		manager_thomas a = new manager_thomas();
+		connexion =  (Connection) a.bdd();
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 550, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(70, 91, 410, 323);
-		frame.getContentPane().add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		
 		JLabel lblNewLabel = new JLabel("Informations générales");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
 		lblNewLabel.setBounds(141, 31, 268, 30);
 		frame.getContentPane().add(lblNewLabel);
+		
+		try {
+			java.sql.Statement stm= connexion.createStatement();
+            Vector data = new Vector();
+            Vector columnsNames = new Vector();
+			// requete pour recuperer les donnees des films
+			ResultSet resultat= stm.executeQuery("SELECT libelle, nom, prenom FROM classe INNER JOIN eleve ON classe.idClasse = eleve.classe");
+		    // Recuperer le titre des colonnes
+            ResultSetMetaData md = (ResultSetMetaData) resultat.getMetaData();
+           
+            // Recuperer le nombre de colonne
+            int columns = md.getColumnCount();
+				 while (resultat.next())
+                 {
+                     Object nb = resultat.getRow();
+                     Vector row = new Vector(columns);
+                     for (int i = 1; i <= columns; i++)
+                     {
+                             row.addElement( resultat.getString("libelle"));
+                             row.addElement( resultat.getString("nom"));
+                             row.addElement( resultat.getString("prenom"));
+                     }
+                     data.addElement( row );
+					 }
+                 // Tout fermer
+                 columnsNames.addElement("classe");
+                 columnsNames.addElement("Nom");
+                 columnsNames.addElement("Prenom");
+                 
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(70, 91, 410, 323);
+        frame.getContentPane().add(scrollPane);
+                 
+		table = new JTable(data, columnsNames);
+		scrollPane.setViewportView(table);
+		
+		
 		
 		JButton btnNewButton = new JButton("Retour");
 		btnNewButton.setBounds(67, 454, 82, 29);
@@ -91,6 +132,10 @@ public class info {
 		JButton btnNewButton_3 = new JButton("Supprimer");
 		btnNewButton_3.setBounds(436, 454, 108, 29);
 		frame.getContentPane().add(btnNewButton_3);
+	}catch(SQLException e1) {
+			e1.printStackTrace();
+			System.out.println("erreur dans l'ajout");
+		}
 	}
 
 	public void run() {
