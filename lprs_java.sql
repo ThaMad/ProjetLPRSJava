dbe8e0edfff24c6970d7f96b182c3049f11d060b
ThaMad <59538761+ThaMad@users.noreply.github.com>
2022-04-05 21:19:24 +0200

    BDD
    BDD à jour

--- lprs_java.sql
+++ lprs_java.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 05, 2022 at 06:02 PM
+-- Generation Time: Apr 05, 2022 at 07:18 PM
 -- Server version: 5.7.32
 -- PHP Version: 8.0.0
 
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `absence` (
   `idAbsence` int(10) NOT NULL,
   `libelle` varchar(25) NOT NULL,
-  `dateD` time NOT NULL,
   `justificatif` text NOT NULL,
   `id_eleve` int(11) NOT NULL,
   `id_prof` int(11) NOT NULL,
-  `dateF` datetime NOT NULL
+  `dateF` datetime NOT NULL,
+  `dateD` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -262,6 +262,14 @@ CREATE TABLE `retard` (
   `justificatif` varchar(50) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `retard`
+--
+
+INSERT INTO `retard` (`idRetard`, `idEleve`, `idProf`, `justificatif`, `date`) VALUES
+(0, 6, 8, 'transport', '2022-04-04 20:38:27'),
+(1, 6, 8, 'vomis', '2022-04-04 20:39:28');
 
 -- --------------------------------------------------------
 
